583ac393e47564e398712b3b2970e88f89211235
rafaelluiis2315 <rafael.barreto@liferay.com>
2022-07-22 12:06:28 -0300

    Aula 3

--- src/App.java
+++ src/App.java
@@ -1,46 +1,38 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         // fazer umn conexao Http e buscar os top filmes
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_1r4uh79j";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        
+/*         String url = "https://api.mocki.io/v2/549a5d8b/MostPopularMovies";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();  */
 
-        //extrair so os dados que eu quero (titulo, poster, classificacao)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        String url = "https://api.nasa.gov/planetary/apod?api_key=Zb2Q1xSHqzuXJq9ZnzXnqnD6qNkDfByuvwYVJyhh&start_date=2022-06-22&end_date=2022-07-22";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+         
 
-        //exibir e manipular os dados
-        for (Map<String,String> filme : listaDeFilmes) {
-            
-            // pegar infomracoes dos filmes
-            String titulo = filme.get("title");
-            String urlImagem = filme.get("image");
-            String ranck = filme.get("imDbRating");
-            
-            // gerar figurinha 
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = titulo + ".png";
+        clienteHttp clienteHttp = new clienteHttp();
+        String json = clienteHttp.buscaDados(url);
 
-            var geradora = new GeradoraDeFigurinhas();
+        // exibir e manipular os dados
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+        var geradora = new GeradoraDeFigurinhas();
+
+        for (int i = 0; i < 3; i++) {
+
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = conteudo.getTitulo() + ".png";
+
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println(titulo);
-            System.out.println(urlImagem);
-            System.out.println(ranck);
+            System.out.println(conteudo.getTitulo());
             System.out.println();
         }
     }
---
+++ src/clienteHttp.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class clienteHttp {
+    
+    public String buscaDados(String url) {
+
+        try {
+
+            URI endereco = URI.create(url);
+            var cliente = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(endereco).GET().build();
+            HttpResponse<String> response = cliente.send(request, BodyHandlers.ofString());
+            return response.body();
+
+
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+}
